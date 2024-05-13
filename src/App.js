b6ff9502995481246ec9f52629104974d08692c0
PRASHANT KUMAR JHA <39798526+PrashantJha3@users.noreply.github.com>
2024-05-13 11:07:27 +0530

    Update App.js With latest changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React , { useState }from 'react';
-import Button from './Button';
+
+import React, { useState } from 'react';
+import './App.css';
 import { ReactComponent as Icon } from './icon.svg';
 import { ReactComponent as DelIcon } from './delete.svg';
-import './App.css';
-const App = () => {
+
+function App() {
   const [inputs, setInputs] = useState(['', '', '']);
 
   const handleInputChange = (index, value) => {
@@ -12,32 +13,39 @@ const App = () => {
     setInputs(newInputs);
   };
 
-  const handleDelete = (index) => {
+  const handleDelete = index => {
     const newInputs = [...inputs];
     newInputs.splice(index, 1);
     setInputs(newInputs);
   };
 
-  
+  const handleAddInput = () => {
+    setInputs([...inputs, '']);
+  };
 
   return (
     <div className="container">
-      <form className="form">
-    {inputs.map((input, index) => (
-      <div key={index} className="input-container">
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => handleInputChange(index, e.target.value)}
-        />
-        <button onClick={() => handleDelete(index)}><DelIcon className="icon" />
+      <div className="form-container">
+        <form className="form">
+          {inputs.map((input, index) => (
+            <div key={index} className="input-container">
+              <input
+                type="text"
+                value={input}
+                onChange={e => handleInputChange(index, e.target.value)}
+                placeholder={`Input ${index + 1}`}
+              />
+              <button type="button" onClick={() => handleDelete(index)}> <DelIcon className="icon" />
       <span className="text">Delete</span></button>
+            </div>
+          ))}
+        </form>
+        <button type="button" className='textAdd' onClick={handleAddInput}><Icon className="icon" />
+      <span className="text">Add</span></button>
       </div>
-    ))}
-    <Button />
-    </form>
-  </div>
+    </div>
   );
-};
+}
 
 export default App;
+
